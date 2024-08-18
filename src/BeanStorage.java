5e2a76fc34d47ea8b2312b40ae6018a3ab3c583c
sujshr <sujshrest@gmail.com>
2024-08-18 17:18:38 +0530

    Removed some code showing abstract classes and method overriding for assignment demonstration

--- src/BeanStorage.java
+++ src/BeanStorage.java
@@ -4,20 +4,4 @@ public class BeanStorage extends Storage {
         super(initialLevel, maxLevel);
     }
 
-    @Override
-    public void checkLevel() {
-        System.out.println("Current bean level: " + getLevel() + " units");
-    }
-
-    @Override
-    public void refill() {
-        super.refill();
-        System.out.println("Bean storage has been refilled.");
-    }
-
-    @Override
-    public boolean use(int amount) {
-        System.out.println("Using " + amount + " units of beans.");
-        return super.use(amount);
-    }
 }
--- src/WaterStorage.java
+++ src/WaterStorage.java
@@ -4,20 +4,4 @@ public class WaterStorage extends Storage {
         super(initialLevel, maxLevel);
     }
 
-    @Override
-    public void checkLevel() {
-        System.out.println("Current water level: " + getLevel() + " units");
-    }
-
-    @Override
-    public void refill() {
-        super.refill();
-        System.out.println("Water storage has been refilled.");
-    }
-
-    @Override
-    public boolean use(int amount) {
-        System.out.println("Using " + amount + " units of water.");
-        return super.use(amount);
-    }
 }
